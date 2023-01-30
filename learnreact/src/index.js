f56c295d7e967e88a72b83e65db5c1f168fafe11
Rohit Jain <rohit@fullstacklearning.com>
2023-01-30 20:13:55 +0530

    after class 30-01-2023

--- learnreact/src/index.js
+++ learnreact/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App'
-import Home from './counter/Home'
+// import Home from './todo/Home'
+import Home from './learningAxios/Home'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const names = ['Ram', "Shyam", 'Seeta', 'Geeta']
-const num = 1
+// const names = ['Ram', "Shyam", 'Seeta', 'Geeta']
+// const num = 1
 
 root.render(
   <>
-    <h1>React Fundamentals</h1>
+    {/* <h1>React Fundamentals</h1> */}
     {/* <App students={names} id={num} /> */}
     <Home />
   </>
